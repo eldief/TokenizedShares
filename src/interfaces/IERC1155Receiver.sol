2129861aebd64734a26f0a2a11180914ea62c6be
LDF <l.defilippi@protonmail.com>
2023-08-10 16:06:16 +0200

    add MIT LICENSE

--- src/interfaces/IERC1155Receiver.sol
+++ src/interfaces/IERC1155Receiver.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.5.0) (token/ERC1155/IERC1155Receiver.sol)
-
 pragma solidity ^0.8.20;
 
 import {IERC165} from "./IERC165.sol";
